2c44eab9c157bdd9832f951b6783d82f365265ab
lizhaotailang <marklztl@sina.cn>
2016-02-19 23:57:52 +0800

    20160219 2357 Tony --> 新增翻译结果可复制功能 其他bug修复

--- app/src/main/java/com/marktony/translator/Activities/MainActivity.java
+++ app/src/main/java/com/marktony/translator/Activities/MainActivity.java
@@ -9,10 +9,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewConfiguration;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -31,6 +31,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -65,6 +66,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initViews();
+
+        //如果有实体菜单按钮，将其屏蔽，强制显示overflow菜单
+        forceShowOverflowMenu();
 
         queue = Volley.newRequestQueue(getApplicationContext());
 
@@ -208,6 +212,21 @@ public class MainActivity extends AppCompatActivity {
 
         queue.add(request);
 
+    }
+
+    private void forceShowOverflowMenu(){
+        ViewConfiguration config = ViewConfiguration.get(this);
+        try {
+            Field menuField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if (menuField != null){
+                menuField.setAccessible(true);
+                menuField.setBoolean(config,false);
+            }
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
 }
