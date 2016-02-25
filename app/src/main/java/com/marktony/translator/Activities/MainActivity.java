9ede9dfa2ced318ea395e9e3c7a8fd5054b250e4
lizhaotailang <marklztl@sina.cn>
2016-02-26 00:27:35 +0800

    20160226 0027 Tony

--- app/src/main/java/com/marktony/translator/Activities/MainActivity.java
+++ app/src/main/java/com/marktony/translator/Activities/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -142,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
 
         etInput = (EditText) findViewById(R.id.et_main_input);
         tvResult = (TextView) findViewById(R.id.tv_show_result);
+
 
     }
 
